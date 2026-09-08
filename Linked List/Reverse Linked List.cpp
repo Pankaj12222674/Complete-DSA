51c28db67a5d52df828a9364722120d8ea39b329
Pankaj Kumar <119557510+Pankaj12222674@users.noreply.github.com>
2026-09-08 18:32:02 +0530

    Implement recursive method to reverse linked list
    Added a recursive approach to reverse a linked list.

--- Linked List/Reverse Linked List.cpp
+++ Linked List/Reverse Linked List.cpp
@@ -18,3 +18,22 @@ public:
         
     }
 };
+
+
+//recursive approach
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head==nullptr || head->next == nullptr){
+            return head;
+           
+        }
+        ListNode* newHead= reverseList(head->next);
+        head->next->next=head;
+        head->next=nullptr;
+        
+        return newHead;
+        
+    }
+};
