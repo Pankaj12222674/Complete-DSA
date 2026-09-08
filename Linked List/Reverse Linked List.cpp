1cea76b10546562d5c19fba1242e9e567efceba4
Pankaj Kumar <119557510+Pankaj12222674@users.noreply.github.com>
2026-09-08 18:22:27 +0530

    Update Reverse Linked List.cpp

--- Linked List/Reverse Linked List.cpp
+++ Linked List/Reverse Linked List.cpp
@@ -1,1 +1,20 @@
 
+//iterative
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        ListNode* res = nullptr;
+        ListNode* curr = head;
+
+        while(curr!=nullptr){
+            ListNode* temp = curr->next;
+            curr->next = res;
+            res = curr;
+            curr = temp;
+        }
+
+        return res;
+        
+    }
+};
